9ca0786b10a13151c817eab04f0d2e1e16de604b
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-23 22:27:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 def speak_to_grandma(string)
   if string = string.downcase
-    puts 'HUH?! SPEAK UP, SONNY!'
+    puts "HUH?! SPEAK UP, SONNY!"
   elseif string.downcase
-    puts 'HUH?! SPEAK UP, SONNY'
+    puts "HUH?! SPEAK UP, SONNY"
   elseif string.upcase
-    puts 'NO, NOT SINCE 1938!'
+    puts "NO, NOT SINCE 1938!"
   elseif string.upcase
-    puts 'NO, NOT SINCE 1938!'
-  elseif string = 'I LOVE YOU GRANDMA'
-    puts 'I LOVE YOU TOO PUMPKIN
+    puts "NO, NOT SINCE 1938!"
+  elseif string = "I LOVE YOU GRANDMA"
+    puts "I LOVE YOU TOO PUMPKIN"
   end
 end
