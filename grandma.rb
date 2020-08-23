5545d59666078c4756e7920bc0a91e560593d3ff
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-23 23:05:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
   return "NO, NOT SINCE 1938!"
 else string == string.downcase
